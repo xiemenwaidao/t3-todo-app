a384e72fa965061a436d35ce7161ae0ff8052d48
xiemenwaidao <menxielijiao@gmail.com>
2023-06-02 18:32:38 +0900

    🚧 section2-8 ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { signOut, useSession } from "next-auth/react";
 import { Auth } from "~/components/Auth";
 import { Layout } from "~/components/Layout";
 import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
+import { TaskForm } from "~/components/TaskForm";
+import { TaskList } from "~/components/TaskList";
 
 const Home: NextPage = () => {
     const { data: session } = useSession();
@@ -18,6 +20,8 @@ const Home: NextPage = () => {
         <Layout title="Todo App">
             <ArrowLeftOnRectangleIcon className="h-6 w-6 cursor-pointer text-blue-600" onClick={() => void signOut()} />
             <p className="my-3 text-xl text-blue-600">{session?.user?.name}</p>
+            <TaskForm />
+            <TaskList />
         </Layout>
     );
 };
